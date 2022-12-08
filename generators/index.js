116cbebd58bb8ae670b3a58a03ec25cf2a1f350b
tor <tormage@hotmail.com>
2022-12-08 12:52:25 +0100

    testing

--- generators/index.js
+++ generators/index.js
@@ -1,3 +1,6 @@
+import updateNotifier from 'update-notifier';
+import packageJson from './package.json' assert { type: 'json' };
+updateNotifier({ pkg: packageJson }).notify();
 const Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
